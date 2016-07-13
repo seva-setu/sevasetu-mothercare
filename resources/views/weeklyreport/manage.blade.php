7ec560938689905806168e3bbbf39aa1699d9c20
shashank srikant <shashank.nit.kurukshetra@gmail.com>
2016-07-13 21:50:07 +0530

    - Fixed table on mycalls page - Added cancel button

--- resources/views/weeklyreport/manage.blade.php
+++ resources/views/weeklyreport/manage.blade.php
@@ -22,7 +22,11 @@ $languagedata= DB::table('mct_language')->where('e_status', 'Active')->orderBy('
 @include('template/admin_title')
 @include('template/admin_cssscripta')
 </head>
-
+<style>
+smallfont{
+	font-size:10px;
+}
+</style>
 <body>
 @include('template/admin_header')
 @include('template/admin_sidebar')
@@ -47,7 +51,7 @@ $languagedata= DB::table('mct_language')->where('e_status', 'Active')->orderBy('
 							<th>{{ trans('routes.uniqueid') }}</th>
 							<th>{{ trans('routes.name') }}</th>
 							<th>{{ trans('routes.location') }}</th>
-							<th>{{ trans('routes.interventionpoint') }}</th>
+							<th colspan="2">{{ trans('routes.interventionpoint') }}</th>
 						</thead>
 						<?php if(!empty($due_list) && count($due_list) > 0) { ?>
 						<tbody>
@@ -64,11 +68,17 @@ $languagedata= DB::table('mct_language')->where('e_status', 'Active')->orderBy('
 								<?php
 									echo(date("d M y", strtotime($value->action_date)));
 								?>
-							</td>
-							<td>
+								<br/>
+								<smallfont>
 								<?php
 									//Write the difference of the dates here.
+									echo "(.. weeks to go)";
 								?>
+								</smallfont>
+							</td>
+							<td>
+								<?php //Should have a confirmation check on this button ?>
+								<a href="" class="btn btn-danger">Cancel</a>
 							</td>
 							<?php //@if(session('user_logged')['v_role']==0 || session('user_logged')['v_role']==1)    <td></td> @endif
 							?>
@@ -78,7 +88,7 @@ $languagedata= DB::table('mct_language')->where('e_status', 'Active')->orderBy('
 								<td colspan="6"><center>{!! $due_list->render() !!}</center></td>
 							</tr>
 						</tbody>
-						<?php} else { ?>
+						<?php } else { ?>
 						<tbody>
 							  <tr>
 								<td colspan="10"><center><em><?php echo trans('routes.norecord'); ?></em></center></td>
@@ -91,16 +101,16 @@ $languagedata= DB::table('mct_language')->where('e_status', 'Active')->orderBy('
 			<div class="col-lg-6 col-md-6">
 				<h3>Calls completed</h3>
 				<table class="table table-striped table-bordered table-hover">
-                        <thead class="success">
+                        <thead class="warning">
 							<th>{{ trans('routes.uniqueid') }}</th>
 							<th>{{ trans('routes.name') }}</th>
 							<th>{{ trans('routes.location') }}</th>
-							<th>{{ trans('routes.interventionpoint') }}</th>
+							<th colspan="2">{{ trans('routes.interventionpoint') }}</th>
 						</thead>
 						<?php if(!empty($due_list) && count($due_list) > 0) { ?>
 						<tbody>
 							<?php foreach ($due_list as $value){ ?>
-							<tr>							
+							<tr>
 							<td data-title="{{ trans('routes.uniqueid') }}"><a href="<?php echo url().'/admin/mycalls/view/'.Hashids::encode($value->due_id); ?>" ><?php echo $value->due_id;?></a></td>
 							
 							<td data-title="{{ trans('routes.name') }}"><a href="<?php echo url().'/admin/mycalls/view/'.Hashids::encode($value->due_id); ?>" ><?php echo $value->name;?></a></td>
@@ -112,11 +122,17 @@ $languagedata= DB::table('mct_language')->where('e_status', 'Active')->orderBy('
 								<?php
 									echo(date("d M y", strtotime($value->action_date)));
 								?>
-							</td>
-							<td>
+								<br/>
+								<smallfont>
 								<?php
 									//Write the difference of the dates here.
+									echo "(.. weeks to go)";
 								?>
+								</smallfont>
+							</td>
+							<td>
+								<?php //Should have a confirmation check on this button ?>
+								<a href="" class="btn btn-danger">Cancel</a>
 							</td>
 							<?php //@if(session('user_logged')['v_role']==0 || session('user_logged')['v_role']==1)    <td></td> @endif
 							?>
@@ -126,10 +142,10 @@ $languagedata= DB::table('mct_language')->where('e_status', 'Active')->orderBy('
 								<td colspan="6"><center>{!! $due_list->render() !!}</center></td>
 							</tr>
 						</tbody>
-						<?php} else { ?>
+						<?php } else { ?>
 						<tbody>
 							  <tr>
-								<td colspan="6"><center><em><?php echo trans('routes.norecord'); ?></em></center></td>
+								<td colspan="10"><center><em><?php echo trans('routes.norecord'); ?></em></center></td>
 							  </tr>
 					  </tbody>
 						  <?php } ?>
