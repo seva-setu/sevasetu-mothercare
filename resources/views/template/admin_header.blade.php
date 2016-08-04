bf1766cef4fc4b92a3a8180fe1889f441582da8e
twinklegupta <twinkle.gupta@yahoo.com>
2016-08-04 15:38:33 +0530

    fixed mobile view

--- resources/views/template/admin_header.blade.php
+++ resources/views/template/admin_header.blade.php
@@ -9,27 +9,35 @@ $userinfo=Session::get('user_logged');
 			<a target="#" href="#">
 				<img class="logo" src="{{ url() }}/assets/img/logo1.jpg" alt="Sevasetu" width="60" height="80">
 			</a>
+			<span class="logout-spn" style="padding-left:10px;border-left:0px;margin-left:0px">
+				<a href="#" style="color:#fff;font-size:20px">Seva Setu's Mother care tool</a>  
+		    </span>
 		</div>
+	   <!-- <div class="navbar-header">
+		  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+		  <span class="icon-bar"></span>
+		  <span class="icon-bar"></span>
+		  <span class="icon-bar"></span>
+		  </button>
+		  
+	   </div> -->
+	   <span class="logout-spn" >
+	   <a href="<?php echo url(); ?>/admin/logout" style="color:#fff;font-size:15px;">Logout</a>  
+	   </span>
+	   <span class="logout-spn" >
+	   <p style="color:#fff;font-size:15px;width:150px;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;">Welcome <?php echo $userinfo['v_user_name'] ?></p>
+	   </span>
 	   <div class="navbar-header">
 		  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
 		  <span class="icon-bar"></span>
 		  <span class="icon-bar"></span>
 		  <span class="icon-bar"></span>
 		  </button>
-		  <span class="logout-spn" >
-			<a href="#" style="color:#fff;">Seva Setu's Mother care tool</a>  
-		  </span>
+		  
 	   </div>
-	   <span class="logout-spn" >
-	   <a href="<?php echo url(); ?>/admin/logout" style="color:#fff;font-size:20px">Logout</a>  
-	   </span>
-	   <span class="logout-spn" >
-	   <p htyle="color:#fff;font-size:20px">Welcome <?php echo $userinfo['v_user_name'] ?></p>
-	   </span>
 	   
 	</div>
  </div>
-
 
 <?php if(isset($userinfo['v_name'])){
 	$role="";
