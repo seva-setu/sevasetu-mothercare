e274e4f8ea3481e7383bafc0e20f13dd8e152757
yogikaushik <kaushikyogesh.95@gmail.com>
2016-08-05 15:34:57 +0530

    admin login view added

---
+++ resources/views/admin/dashboard1.blade.php
@@ -0,0 +1,21 @@
+<?php 
+	$userinfo = Session::get('user_logged');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+@include('template/admin_title')
+@include('template/admin_cssscripta')
+</head>
+<style>
+smallfont{
+	font-size:10px;
+}
+</style>
+<body>
+@include('template/admin_header')
+@include('template/admin_sidebar_new')
+helloddo
+</body>
+</html>
