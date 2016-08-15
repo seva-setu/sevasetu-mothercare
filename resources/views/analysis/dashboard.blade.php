b123c42a7309bf30c0c5399570ff491d2908d400
twinklegupta <twinkle.gupta@yahoo.com>
2016-08-15 11:50:07 +0530

    admin dashboard changes

--- resources/views/analysis/dashboard.blade.php
+++ resources/views/analysis/dashboard.blade.php
@@ -18,43 +18,42 @@ smallfont{
 
 <div id="page-wrapper" >
 	<div id="page-inner">
-		<div class="row">
-			<div class="col-lg-10 col-md-10">
-			<h3>
-					Total unsuccessful calls : <?php echo $total_calls;?>
-			</h3>
 		
-			</div>
-		</div>
 		<div class="row">
 			<div class="col-lg-10 col-md-10">
 			<h3>
-					Call champions who did not make scheduled calls
+					Call champions who have calls in the week from <?php echo date("j F Y", $datestart); ?> to <?php echo date("j F Y", $dateend); ?>
 			</h3>
 			<?php 
-			if(!empty($cc_not_called)){
+			if(!empty($call_details)){
 			?>
 			<table class="table table-striped table-bordered table-hover">
 					<thead>
 					   <tr>
-					   	   <th> ID </th>
-						   <th> Name </th>
+					   	   <th> Call ID </th>
+						   <th> Call Champion Name </th>
+						   <th> Mother's Name </th>
+						   <th> Date of Call </th>
+						   <th> Mother's number </th>
 					   </tr>
 					 </thead>
 					 <tbody>
 					<?php 
-					foreach($cc_not_called as $val)
+					foreach($call_details as $val1)
 					{
-						foreach($val as $val1){
+						
 					?>
 					<tr>
-					<td> {{$val1->ID }} </td>
-					<td> {{$val1->name}} </td>
+					<td> {{$val1->due_id }} </td>
+					<td> {{$val1->c_name}} </td>
+					<td> {{$val1->b_name}} </td>
+					<td> {{$val1->dt_intervention_date}} </td>
+					<td> {{$val1->v_phone_number}} </td>
 					</tr>
 					 </tbody>
 					 
 					 <?php 
-						}
+						
 					}
 			?>
 			
@@ -63,7 +62,7 @@ smallfont{
 			}
 			else {
 					 ?>
-					 <h4>No pending calls in the past week</h4>
+					 <h4>No scheduled calls in the past week</h4>
 					 <?php 
 			}
 					 ?>
@@ -71,37 +70,85 @@ smallfont{
 			
 		</div>
 		
+		
 		<div class="row">
 			<div class="col-lg-10 col-md-10">
 			<h3>
-					Calls scheduled last week
+					Statistics for the week from <?php echo date("j F Y", $datestart); ?> to <?php echo date("j F Y", $dateend); ?>
+			</h3>
+			<table class="table table-striped table-bordered table-hover">
+			<tr>
+			<td> Number of calls made </td>
+			<td> {{$totalcalls[0]->count}} </td>
+			</tr>
+			<tr>
+			<td> Number of mothers assigned </td>
+			<td> {{$mothersassigned[0]->count}} </td>
+			</tr>
+			<tr>
+			<td> Average calls made per mother </td>
+			<td> {{$averagepermother[0]->Average}} </td>
+			</tr>
+			<tr>
+			<td> Number of scheduled calls connected in the first call attempt  </td>
+			<td> {{$callsattemptequal1[0]->count}} </td>
+			</tr>
+			<tr>
+			<td> Number of scheduled calls for which two attempts were made </td>
+			<td> {{$callsattemptequal2[0]->count}} </td>
+			</tr>
+			<tr>
+			<td> Number of scheduled calls for which more than 2 attempts were made </td>
+			<td> {{$callsattemptgt2[0]->count}} </td>
+			</tr>
+			<tr>
+			<td> Number of mothers whose phone numbers were marked incorrect </td>
+			<td> {{$incorrectphno[0]->count}} </td>
+			</tr>
+			<tr>
+			<td> Number of mothers who were not reachable or were out of network </td>
+			<td> {{$notreachable[0]->count}} </td>
+			</tr>
+			<tr>
+			<td> Number of action items generated </td>
+			<td> {{$actionitems[0]->count}} </td>
+			</tr>
+			</table>
+			</div>
+		</div>
+		
+		
+		
+		
+		
+		<div class="row">
+			<div class="col-lg-10 col-md-10">
+			<h3>
+					Call champions who have not made scheduled calls in the week from <?php echo date("j F Y", $datestart); ?> to <?php echo date("j F Y", $dateend); ?>
 			</h3>
 			<?php 
-			if(!empty($call_details)){
+			if(!empty($cc_not_called)){
 			?>
 		    <table class="table table-striped table-bordered table-hover">
 					<thead>
 					   <tr>
 					   	   <th> Due ID </th>
-						   <th> Beneficiary ID </th>
-						   <th> Call Champion ID </th>
-						   <th> Action ID </th>
-						   <th> Intervention Date</th>
-						   <th> Reminder status </th>
+						   <th> Name </th>
+						   <th> Call Champion Phone Number </th>
+						   <th> Reminder Status </th>
+						   
 					   </tr>
 					 </thead>
 					 <tbody>
 					<?php 
-					foreach($call_details as $val)
+					foreach($cc_not_called as $val)
 					{
 						
 					?>
 					<tr>
 					<td> {{$val->due_id }} </td>
-					<td> {{$val->fk_b_id}} </td>
-					<td> {{$val->fk_cc_id}} </td>
-					<td> {{$val->fk_action_id}} </td>
-					<td> {{$val->dt_intervention_date}} </td>
+					<td> {{$val->name}} </td>
+					<td> {{$val->phno}} </td>					
 					<td> {{$val->reminder_status}} </td>
 					</tr>
 					 </tbody>
