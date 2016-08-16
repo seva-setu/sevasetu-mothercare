e3048811cfc2e612e8ed3a46f53e8f7262cf17ca
yogikaushik <kaushikyogesh.95@gmail.com>
2016-08-16 17:08:45 +0530

    header src files dwonloaded and made static assets

--- resources/views/admin/callchampions.blade.php
+++ resources/views/admin/callchampions.blade.php
@@ -7,8 +7,8 @@
 <head>
 	@include('template/admin_title')
 	@include('template/admin_cssscripta')
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>	
+	<script src="{{ url() }}/assets/js_admin/jquery-1.12.4.js"></script>
+	<script src="{{ url() }}/assets/js_admin/bootstrap-3.3.7.min.js"></script>	
 </head>
 <style>
 	smallfont{
