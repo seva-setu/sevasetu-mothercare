062d586e4745f31d74e9878f416115f4bd597281
shub2801 <shubham.malhotra@mindscroll.com>
2017-01-08 22:04:42 +0530

    dt_modify_dates

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -277,57 +277,53 @@ class AdminController extends Controller{
   */
 
   public function action_items(){
-    $data=DB::table('mct_callchampion_report')->get();//->where('status',0)->get();
+     $data=DB::table('mct_callchampion_report')->get();//->where('status',0)->get();
      $x=0;
      $y=0;
      $alread_resolved=0;
      foreach($data as $i)
      {
-      //status=1 implies that action items are already resolved.
+        
+        //status=1 implies that action items are already resolved.
         if($i->status==1)
         {
           $alread_resolved++;
         }
         $due_id=DB::table('mct_due_list')->where('due_id',$i->fk_due_id)->first();
         $cc_id=DB::table('mct_due_list')->where('due_id',$i->fk_due_id)->first()->fk_cc_id;
-        //$newdata[$x]['b_id']=$due_id->fk_b_id;
         $field_worker_id=DB::table('mct_beneficiary')->where('b_id',$due_id->fk_b_id)->first()->fk_f_id;
         $b_id=DB::table('mct_beneficiary')->where('b_id',$due_id->fk_b_id)->first();
-        // dd();
-       $newdata[$x]['cc_id']='';
+        $newdata[$x]['cc_id']='';
         $newdata[$x]['cc_user_id']='';
         $newdata[$x]['call_champion_name']='';
-
         if($cc_id != NULL)
         {
           $cc_user_id=DB::table('mct_call_champions')->where('cc_id',$cc_id)->first()->fk_user_id;
           $cc_user_id=DB::table('mct_call_champions')->where('cc_id',$cc_id)->first()->fk_user_id;
-        $newdata[$x]['cc_id']=$cc_id;
-        $newdata[$x]['cc_user_id']=$cc_user_id;
-        $newdata[$x]['call_champion_name']=DB::table('mct_user')->where('user_id',$cc_user_id)->first()->v_name;
- 
+          $newdata[$x]['cc_id']=$cc_id;
+          $newdata[$x]['cc_user_id']=$cc_user_id;
+          $newdata[$x]['call_champion_name']=DB::table('mct_user')->where('user_id',$cc_user_id)->first()->v_name; 
         }
-
         $field_worker_user_id=DB::table('mct_field_workers')->where('f_id',$field_worker_id)->first()->fk_user_id;
         if(($i->t_action_items!=''||$i->t_conversation!=''))
         {
-          if($i->t_action_items!='')
-          {
-            $y++;
-          }
-        $newdata[$x]['field_worker_name']=DB::table('mct_user')->where('user_id',$field_worker_user_id)->first()->v_name;
-        $newdata[$x]['action_items']=$i->t_action_items;    
-        $newdata[$x]['beneficiary_id']=$b_id->b_id;
-        $newdata[$x]['beneficiary_name']=$b_id->v_name;
-        $newdata[$x]['beneficiary_village']=$b_id->v_village_name;
-        $newdata[$x]['beneficiary_contact']=$b_id->v_phone_number;      
-        $newdata[$x]['date_generated']=$due_id->dt_intervention_date;
-        $newdata[$x]['call_id']=$i->fk_due_id;
-        $newdata[$x]['report_id']=$i->report_id;
-        $newdata[$x]['notes']=$i->t_conversation;
-        $newdata[$x]['e_call_status']=$i->e_call_status;
-        $newdata[$x]['status']=$i->status;
-        $x++;          
+            if($i->t_action_items!='')
+            {
+              $y++;
+            }
+          $newdata[$x]['field_worker_name']=DB::table('mct_user')->where('user_id',$field_worker_user_id)->first()->v_name;
+          $newdata[$x]['action_items']=$i->t_action_items;    
+          $newdata[$x]['beneficiary_id']=$b_id->b_id;
+          $newdata[$x]['beneficiary_name']=$b_id->v_name;
+          $newdata[$x]['beneficiary_village']=$b_id->v_village_name;
+          $newdata[$x]['beneficiary_contact']=$b_id->v_phone_number;      
+          $newdata[$x]['date_generated']=$i->dt_modify_date;//$due_id->dt_intervention_date;
+          $newdata[$x]['call_id']=$i->fk_due_id;
+          $newdata[$x]['report_id']=$i->report_id;
+          $newdata[$x]['notes']=$i->t_conversation;
+          $newdata[$x]['e_call_status']=$i->e_call_status;
+          $newdata[$x]['status']=$i->status;
+          $x++;          
         }
         // $x represents total entries.
         //$already resolved represents resolved actions.
